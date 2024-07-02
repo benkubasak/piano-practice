0e3810136cd5cbdd54547fc8f6a5ea37f7493af7
BEN KUBASAK <benkubasak@users.noreply.github.com>
2024-07-02 17:45:34 -0500

    Add files via upload

---
+++ src/js/default.js
@@ -0,0 +1,78 @@
+function loadJSONContent() {
+    function generateHTML(data) {
+        const body = document.body;
+        const title = document.createElement('h1');
+        title.textContent = data.title;
+        body.appendChild(title);
+
+        data.sections.forEach(section => {
+            const sectionElement = document.createElement('section');
+            sectionElement.setAttribute('style', `background: ${sanitizeColor(section.color)}`);
+
+            const h2 = document.createElement('h2');
+            h2.textContent = sanitizeText(section.title);
+            sectionElement.appendChild(h2);
+
+            const ul = document.createElement('ul');
+            section.items.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = sanitizeText(item);
+                ul.appendChild(li);
+            });
+            sectionElement.appendChild(ul);
+
+            body.appendChild(sectionElement);
+        });
+    }
+
+    function sanitizeText(input) {
+        const div = document.createElement('div');
+        div.appendChild(document.createTextNode(input));
+        return div.innerText;
+    }
+    
+    function sanitizeColor(input) {
+        let output = "silver";
+        switch(input) {
+            case "red":
+                output = "red";
+                break;
+            case "orange":
+                output = "darkorange";
+                break;
+            case "yellow":
+                output = "gold";
+                break;
+            case "green":
+                output = "green";
+                break;
+            case "blue":
+                output = "mediumblue";
+                break;
+            case "indigo":
+                output = "indigo";
+                break;
+            case "violet":
+                output = "violet";
+                break;
+        }
+        return output;
+    }
+  
+    fetch('/src/json/default.json')
+        .then(response => response.json())
+        .then(data => {
+            generateHTML(data);
+        })
+        .catch(error => {
+            console.error('Error loading JSON data:', error);
+        });
+}
+
+function loadPage() {
+    loadJSONContent();
+}
+
+if (document.readyState != 'loading') {loadPage();}
+else if (document.addEventListener) {document.addEventListener('DOMContentLoaded', loadPage);}
+else {document.attachEvent('onreadystatechange', function() {if (document.readyState=='complete') {loadPage();}});}
